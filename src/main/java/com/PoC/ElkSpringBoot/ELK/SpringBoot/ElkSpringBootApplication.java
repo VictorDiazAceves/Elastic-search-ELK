47edce2056a3d2e33857b5153bbdbf999ad215f7
VictorDiaz <kqkt675@astrazeneca.net>
2023-03-07 12:09:44 -0600

    Primer commit ELK

---
+++ src/main/java/com/PoC/ElkSpringBoot/ELK/SpringBoot/ElkSpringBootApplication.java
@@ -0,0 +1,15 @@
+package com.PoC.ElkSpringBoot.ELK.SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElkSpringBootApplication.class, args);
+	}
+
+}
+
+
---
+++ src/test/java/com/PoC/ElkSpringBoot/ELK/SpringBoot/ElkSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PoC.ElkSpringBoot.ELK.SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElkSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
